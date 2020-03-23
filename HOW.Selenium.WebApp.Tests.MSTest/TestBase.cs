d67765e2dc0fca14c71f6bc6a08cd6b144a48d3a
Kevin Kraus <kevin.kraus@outlook.com>
2020-03-23 15:10:48 -0600

    updated solution with new projects and finalized session1

---
+++ HOW.Selenium.WebApp.Tests.MSTest/TestBase.cs
@@ -0,0 +1,27 @@
+﻿using HOW.Selenium.WebApp.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HOW.Selenium.WebApp.Tests.MSTest
+{
+    public class TestBase
+    {
+        //Snippet selbase
+        [TestInitialize()]
+        public void Initialize()
+        {
+            Driver.Initialize(
+                TestContext.Properties["TargetBrowser"].ToString(),
+                bool.Parse(TestContext.Properties["isPrivateMode"].ToString()),
+                bool.Parse(TestContext.Properties["isHeadless"].ToString()));
+            Driver.BaseUrl = TestContext.Properties["BaseUrl"].ToString();
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            Driver.Quit();
+        }
+
+        public TestContext TestContext { get; set; }
+    }
+}
