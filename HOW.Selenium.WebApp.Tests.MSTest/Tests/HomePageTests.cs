c61245d09a5a117f640612303acdd9b7927321ba
Kevin Kraus <kevin.kraus@outlook.com>
2020-03-24 15:08:12 -0600

    added core session3 changes

---
+++ HOW.Selenium.WebApp.Tests.MSTest/Tests/HomePageTests.cs
@@ -0,0 +1,17 @@
+﻿using HOW.Selenium.WebApp.Framework.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HOW.Selenium.WebApp.Tests.MSTest.Tests
+{
+    [TestClass]
+    public class HomePageTests : TestBase
+    {
+        [TestMethod]
+        public void HomePage_Navigate_To_Page()
+        {
+            HomePage.GoTo();
+
+            Assert.IsTrue(HomePage.IsAt);
+        }
+    }
+}
