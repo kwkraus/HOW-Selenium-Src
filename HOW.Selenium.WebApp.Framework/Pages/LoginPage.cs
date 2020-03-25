da3facb2469567a9574bbfc58f0b8e73dff236cb
Kevin Kraus <kevin.kraus@outlook.com>
2020-03-25 14:52:14 -0600

    completed the login page object and login test

---
+++ HOW.Selenium.WebApp.Framework/Pages/LoginPage.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+
+namespace HOW.Selenium.WebApp.Framework.Pages
+{
+    public class LoginPage
+    {
+        public static void GoTo()
+        {
+            Driver.Instance.Navigate().GoToUrl($"{Driver.BaseUrl}/Identity/Account/Login");
+        }
+
+        public static bool IsAt
+        {
+            get
+            {
+                var header = Driver.Instance.FindElement(By.TagName("h1"));
+
+                return (header.Text == "Log in");
+            }
+        }
+
+        public static void Login(string userName, string password)
+        {
+            var loginElement = Driver.Instance.FindElement(By.Id("Input_Email"));
+            loginElement.Clear();
+            loginElement.SendKeys(userName);
+
+            Driver.Instance.FindElement(By.Id("Input_Password")).SendKeys(password);
+
+            loginElement.Submit();
+        }
+    }
+}
---
+++ HOW.Selenium.WebApp.Tests.MSTest/Tests/LoginPageTests.cs
@@ -0,0 +1,17 @@
+﻿using HOW.Selenium.WebApp.Framework.Pages;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HOW.Selenium.WebApp.Tests.MSTest.Tests
+{
+    [TestClass]
+    public class LoginPageTests : TestBase
+    {
+        [TestMethod]
+        public void LoginPage_Attempt_Successful_Login()
+        {
+            LoginPage.GoTo();
+            LoginPage.Login("a@a.com", "Pass@word1");
+            Assert.IsTrue(HomePage.IsAt, "Failed to Login and redirect to HomePage");
+        }
+    }
+}
