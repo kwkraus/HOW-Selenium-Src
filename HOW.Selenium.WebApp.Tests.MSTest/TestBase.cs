249f3608452796419d9508575d30c74f9b09e131
Kevin Kraus <kevin.kraus@outlook.com>
2020-03-25 12:46:38 -0600

    readded testbase

---
+++ HOW.Selenium.WebApp.Tests.MSTest/TestBase.cs
@@ -0,0 +1,28 @@
+﻿using HOW.Selenium.WebApp.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HOW.Selenium.WebApp.Tests.MSTest
+{
+    public class TestBase
+    {
+
+        public TestContext TestContext { get; set; }
+
+        //Snippet selbase
+        [TestInitialize()]
+        public void Initialize()
+        {
+            Driver.Initialize(
+                TestContext.Properties["TargetBrowser"].ToString(),
+                bool.Parse(TestContext.Properties["isPrivateMode"].ToString()),
+                bool.Parse(TestContext.Properties["isHeadless"].ToString()));
+            Driver.BaseUrl = TestContext.Properties["BaseUrl"].ToString();
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            Driver.Quit();
+        }
+    }
+}
